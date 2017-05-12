e0f36f6d7751a20073b17baa0935dbe56fece640
TsubasaK111 <Tsubasa.K111@gmail.com>
2017-05-12 17:42:04 +0900

    fix: update sample output format to more verbose version

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-import sys
-from app.main import main
+import argparse
+from app.app import main
 
-main(sys.argv[1:])
+parser = argparse.ArgumentParser(description='Write description here')
+parser.add_argument('basic_arguments', nargs='*')
+
+args = parser.parse_args()
+
+main(args.basic_arguments, args)
